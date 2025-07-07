5d4ac7d61c7fd205bc24bb5351268c6a87f3b45e
chaimabenayed <chaimabenayed451@gmail.com>
2025-07-07 20:10:32 +0200

    fusion avec resolution de conflit

--- projet.py
+++ projet.py
@@ -1,6 +1,8 @@
+<<<<<<< HEAD
+
 <<<<<<< HEAD 
 exercie examen 2025 def produit(T):
-
+examne de  tp ch 
  S = 0
  for t in T:
     S ++= t
@@ -9,4 +11,13 @@ exercie examen 2025 def produit(T):
 data = [1,3,5]
 moy = statistics.mean(data)
 print ('la moyenne est moy')
+>>>>>>> project1
+=======
+daata = [1,35]
+print("la moyenne est :" Moy(data))
+print("le min est :" min(data))
+print("le max est :" max(data))
+
+esle : 
+print ("dossier  vide")
 >>>>>>> project1
